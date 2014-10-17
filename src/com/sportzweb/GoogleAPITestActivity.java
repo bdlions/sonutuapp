aa3c972c0218bded17aaba0c0b1971e3d1836f2e
Alamgir Kabir <alamgirkabir7@gmail.com>
2014-10-17 23:36:20 +0600

    move away google api in a library project

--- src/com/sportzweb/GoogleAPITestActivity.java
+++ src/com/sportzweb/GoogleAPITestActivity.java
@@ -15,7 +15,6 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
-
 public class GoogleAPITestActivity extends Activity {
 
 	private GraphicalView mChart;
